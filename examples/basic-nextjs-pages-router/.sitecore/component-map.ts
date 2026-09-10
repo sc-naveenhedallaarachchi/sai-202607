a72bd2214d62156dde563b9af2d381f571279ba8
sc-naveenhedallaarachchi <nvhd@sitecore.net>
2026-09-10 11:50:15 +0530

    test

--- examples/basic-nextjs-pages-router/.sitecore/component-map.ts
+++ examples/basic-nextjs-pages-router/.sitecore/component-map.ts
@@ -4,18 +4,23 @@ import { BYOCWrapper, NextjsContentSdkComponent, FEaaSWrapper } from '@sitecore-
 import { Form } from '@sitecore-content-sdk/nextjs';
 
 // end of built-in components
+import * as RichText from 'src/components/rich-text/RichText';
+import * as richtextprops from 'src/components/rich-text/rich-text.props';
 import * as PartialDesignDynamicPlaceholder from 'src/components/partial-design-dynamic-placeholder/PartialDesignDynamicPlaceholder';
-import * as Image from 'components/image/ImageBK2';
+import * as Imageaa from 'src/components/image/Imageaa';
+import * as Image from 'src/components/image/Image';
 import * as imageprops from 'src/components/image/image.props';
 
 export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCWrapper],
   ['FEaaSWrapper', FEaaSWrapper],
   ['Form', Form],
+  ['RichText', { ...RichText }],
+  ['rich-text', { ...richtextprops }],
   ['PartialDesignDynamicPlaceholder', { ...PartialDesignDynamicPlaceholder }],
+  ['Imageaa', { ...Imageaa }],
   ['Image', { ...Image }],
   ['image', { ...imageprops }],
 ]);
 
 export default componentMap;
-
